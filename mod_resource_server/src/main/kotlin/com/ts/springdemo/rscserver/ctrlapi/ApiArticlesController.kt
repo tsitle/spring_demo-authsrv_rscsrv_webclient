d0884465e501aedd5e781a16e85bb8378b5114b8
Thomas <technisandk@gmail.com>
2022-08-15 14:57:17 +0200

    RSCSERVER: Add support for retrieving other user's Articles with Client Credentials Grant

--- mod_resource_server/src/main/kotlin/com/ts/springdemo/rscserver/ctrlapi/ApiArticlesController.kt
+++ mod_resource_server/src/main/kotlin/com/ts/springdemo/rscserver/ctrlapi/ApiArticlesController.kt
@@ -1,16 +1,21 @@
 package com.ts.springdemo.rscserver.ctrlapi
 
+import com.ts.springdemo.common.constants.AuthScope
+import com.ts.springdemo.common.entityapi.ApiCrudRequestReadArticle
 import com.ts.springdemo.common.entityapi.ApiDataArticle
 import com.ts.springdemo.common.entityapi.ApiCrudResponseCreate
 import com.ts.springdemo.common.entityapi.ApiCrudResponseRead
 import com.ts.springdemo.rscserver.entity.DbDataArticle
 import com.ts.springdemo.rscserver.repository.DbDataArticleRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 
 @RestController
@@ -19,17 +24,38 @@ class ApiArticlesController(
 			private val dbDataArticleRepository: DbDataArticleRepository
 		) {
 
+	@Throws(ResponseStatusException::class)
 	@GetMapping("/api/v1/articles")
-	fun getArticles(): ApiCrudResponseRead<ApiDataArticle> {
+	fun getArticles(@RequestBody queryParamsJson: ApiCrudRequestReadArticle?): ApiCrudResponseRead<ApiDataArticle> {
 		val authentication = SecurityContextHolder.getContext().authentication
-		val dbArticles: List<DbDataArticle> = if (! authentication?.name.isNullOrEmpty()) {
-				dbDataArticleRepository.findByUserId(authentication.name)
+				?: throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "OAuth2 Login required")
+		val authenticatedUserName = authentication.name ?: ""
+		val isAllowedToReadOtherUsersData = authentication.authorities.contains(
+					SimpleGrantedAuthority(
+							AuthScope.buildAuthScope(AuthScope.EnScopes.ACCESS_READ_OTHER_USERS_DATA)
+						)
+			)
+		val userEmail: String? = if (queryParamsJson != null && queryParamsJson.userEmail.isNotEmpty()) {
+				queryParamsJson.userEmail
 			} else {
-				return ApiCrudResponseRead.Companion.Builder<ApiDataArticle>()
-						.ok(false)
-						.error("no authentication found")
-						.build()
+				authenticatedUserName.ifEmpty { null }
 			}
+		//
+		if (userEmail.isNullOrEmpty() || ! userEmail.contains("@")) {
+			return ApiCrudResponseRead.Companion.Builder<ApiDataArticle>()
+					.ok(false)
+					.error("no valid authentication found")
+					.build()
+		}
+		if (! isAllowedToReadOtherUsersData && userEmail != authenticatedUserName) {
+			// we could also throw Error 403 (FORBIDDEN) here
+			return ApiCrudResponseRead.Companion.Builder<ApiDataArticle>()
+					.ok(false)
+					.error("not allowed to read other user's data")
+					.build()
+		}
+		//
+		val dbArticles: List<DbDataArticle> = dbDataArticleRepository.findByUserId(userEmail)
 		val listApiData: MutableSet<ApiDataArticle> = mutableSetOf()
 		dbArticles.forEach {
 				listApiData.add(it.toApiDataArticle())
---
+++ mod_z_common/src/main/kotlin/com/ts/springdemo/common/entityapi/ApiCrudRequestReadArticle.kt
@@ -0,0 +1,6 @@
+package com.ts.springdemo.common.entityapi
+
+@Suppress("unused")
+class ApiCrudRequestReadArticle(
+			var userEmail: String
+		)
